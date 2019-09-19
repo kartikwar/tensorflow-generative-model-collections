f2bd16170e3a961eca9bb3af9efb6151825fe825
kartikwar <kartiksirwani@gmail.com>
2019-09-19 14:38:01 +0530

    distance calculation now being done in inference

--- inference.py
+++ inference.py
@@ -1,8 +1,12 @@
 import tensorflow as tf
 from utils import *
 import os
+import json
+import numpy as np
 
 def get_encoded_vectors(sess, op_dict, folder_path):
+
+    encoded_vectors = {}
 
     X = np.zeros(shape=(64, 224, 224, 3))
     y = np.zeros(shape=(64,))
@@ -18,11 +22,39 @@ def get_encoded_vectors(sess, op_dict, folder_path):
         img_index += 1
 
     mu, sigma = sess.run([op_dict['mu'], op_dict['sigma']], feed_dict={op_dict['input']: X})
+
+    for index in range(64):
+        im_name = all_docs[index]
+        mu_ = mu[index]
+        encoded_vectors[im_name] = mu_.tolist()
+
+    return encoded_vectors
+
+
+def calculate_distance(a, b):
+    a = np.array(a)
+    b = np.array(b)
+    dist = np.linalg.norm(a-b)
+    return dist
+
+def debug():
+    with open('template_vectors.json', 'r') as vecf:
+        vectors = json.load(vecf)
+    # dist1 = calculate_distance(vectors["7_35"], vectors["7_42"])
+    dist2 = calculate_distance(vectors["020513314_1812.pdf0.jpg"], vectors["020513314_1812.pdf2.jpg"])
+    dist3 = calculate_distance(vectors["032147968_1901.pdf0.jpg"], vectors["032147799_1905.pdf0.jpg"])
+    # dist4 = calculate_distance(vectors["0_3"], vectors["0_4"])
+    # dist5 = calculate_distance(vectors["0_9"], vectors["0_4"])
+    # dist6 = calculate_distance(vectors["0_29"], vectors["0_10"])
     pass
 
-
-if __name__ == "__main__":
+def run_inference():
     sess, op_dict = get_ops('/Users/kartik/personal/tensorflow-generative-model-collections/checkpoint/VAE_documents_64_8/VAE/VAE.model-113')
     folder_path="/Users/kartik/Documents/ami_invoices_sample/" 
-    get_encoded_vectors(sess, op_dict, folder_path)
-    pass
+    vectors = get_encoded_vectors(sess, op_dict, folder_path)
+    with open('template_vectors.json', 'w') as tv:
+        json.dump(vectors, tv)
+
+if __name__ == "__main__":
+    # run_inference()
+    debug()
